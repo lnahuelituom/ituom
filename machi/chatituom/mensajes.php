6278794b68ff0a4190a16f6a1acdb4e75dcd4018
MACHI <marcelod.salinas@gmail.com>
2014-08-20 21:17:02 -0300

    prueba machi

--- machi/chatituom/mensajes.php
+++ machi/chatituom/mensajes.php
@@ -3,17 +3,17 @@
 $con=mysqli_connect("ftp.proyectosarcadius.com.ar","proyectosarcad03","Lanquel7","chat");
 // Check connection
 if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-  
+{
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 $result = mysqli_query($con,"SELECT * FROM mensajes");
 
 while($row = mysqli_fetch_array($result))
-  {
-  echo "<b>".$row['usuario']."</b>: ".$row['contenido'];
-  echo "<hr>";
-  }
+{
+    echo "<b>".$row['usuario']."</b>: ".$row['contenido'];
+    echo "<hr>";
+}
 
 mysqli_close($con);
 
